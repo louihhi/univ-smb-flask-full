264343a197c388bb3f8db5ed204e8973bbdffe08
louihhi be <berthierlouis@live.Fr>
2023-03-24 11:17:48 +0000

    tp6/louisBerthier

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash, session, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, API!"
+@app.route('/')
+def login():
+    url = 'https://5001-louihhi-univsmbflaskful-obxdyg3fi4i.ws-eu92.gitpod.io/'
+    username = request.args.get('username')
+    password = request.args.get('password')
+    print(f"toto {username}")
+
+    if username == 'admin' and password == 'admin':
+        data = 'Connecté avec succès'
+        return jsonify({'success': True, 'message': 'Connecté avec succès'})
+    else:
+        data = 'Nom d\'utilisateur ou mot de passe incorrect'
+        return  jsonify({'success': False, 'message': 'Nom d\'utilisateur ou mot de passe incorrect}'})
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -1,8 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash, session, jsonify
+from flask_mysqldb import MySQL
+import requests
+from flask_mysqldb import MySQL
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'louis'
+app.config['MYSQL_PASSWORD'] = 'louis'
+app.config['MYSQL_DB'] = 'identity'
+mysql = MySQL(app)
 
 app = Flask(__name__)
 
-@app.route("/")
-def start(): 
-    return render_template('start.html')
-    
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' :
+        url = 'http://localhost:5000/'
+        username = request.form['username']
+        password = request.form['password']
+        data = {'username': username, 'password': password}
+
+        #cur = mysql.connection.cursor()
+        #cur.execute("SELECT motdepasse FROM auth WHERE user="+username + ";")
+        #rows = cur.fetchall()
+
+        response = requests.get(url, params=data)
+        # Traitez la réponse de la requête ici
+        print(response)
+        return response.json()
+        
+
+    # Le code suivant est exécuté si la méthode de requête est GET
+    #return render_template('start.html')
+        
+
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
