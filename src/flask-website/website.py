1cde6909e8b7b77e662b38aca748b01cf0ed1375
louihhi be <berthierlouis@live.Fr>
2023-03-30 16:06:44 +0000

    fin tp final

--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, session, jsonify
-from flask_mysqldb import MySQL
-import requests
-from flask_mysqldb import MySQL
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'louis'
-app.config['MYSQL_PASSWORD'] = 'louis'
-app.config['MYSQL_DB'] = 'identity'
-mysql = MySQL(app)
+import requests
+
 
 app = Flask(__name__)
+
+app.secret_key='toto:tata'
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -19,19 +17,18 @@ def login():
         password = request.form['password']
         data = {'username': username, 'password': password}
 
-        #cur = mysql.connection.cursor()
-        #cur.execute("SELECT motdepasse FROM auth WHERE user="+username + ";")
-        #rows = cur.fetchall()
+    
 
         response = requests.get(url, params=data)
-        # Traitez la réponse de la requête ici
-        print(response)
-        return response.json()
+        if response.text == "ok":
+            session['username'] = username
+            return redirect(url_for('layout'))
+        else: 
+            return redirect('/')
+
+        
         
 
-    # Le code suivant est exécuté si la méthode de requête est GET
-    #return render_template('start.html')
-        
 
     return '''
         <form method="post">
@@ -41,5 +38,35 @@ def login():
         </form>
         '''
 
+@app.route('/start')
+def start():
+    if 'username' in session:
+        
+        return render_template('start.html')
+    else:
+        return redirect(url_for('login'))
+    
+    
+
+@app.route('/layout', methods=['GET', 'POST'])
+def layout():
+    if 'username' in session:
+        url = 'http://localhost:5000/users'
+        data = {"users":"all"}
+        response = requests.get(url, params=data)
+        datas = response.json()
+        print(type(datas))
+        return render_template('layout.html', datas=datas)
+    else:
+        return redirect(url_for('login'))
+    
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
+
